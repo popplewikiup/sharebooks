0c7aa44e8c2b3852d77eef0fd9b2cf84cce4cd15
popplewikiup <start_2018@sina.com>
2017-11-27 02:05:08 +0800

    First Commit

---
+++ sharebooks/src/main/java/com/xin/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.xin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xin")
+public class RootConfig {
+
+}
